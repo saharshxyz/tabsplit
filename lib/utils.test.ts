92200de9f3d8ad09eff72fe02b0c9b0f8ae1192e
Saharsh Yeruva <yeruva@saharsh.xyz>
2024-08-19 05:28:36 -0400

    consolidate util functions

--- lib/utils.test.ts
+++ lib/utils.test.ts
@@ -1,5 +1,5 @@
-import { logZodErrors, createSplitURL } from "@/lib/utils"
-import { formSchema, FormSchema } from "@/lib/schemas"
+import { logZodErrors, createSplitURL, calculateSplit } from "@/lib/utils"
+import { formSchema, FormSchema, splitSchema } from "@/lib/schemas"
 
 describe("createSplitURL", () => {
   it("should create a correctly formatted and encoded URL", () => {
@@ -123,6 +123,146 @@ describe("createSplitURL", () => {
       const result = createSplitURL(input)
 
       console.log(result[0])
+    }
+  })
+})
+
+describe("calculateSplit", () => {
+  it("should accept valid input and produce correctly formatted output", () => {
+    const inputData = {
+      checkName: "Dinner at Joe's",
+      taxAmount: 5.0,
+      tipBeforeTax: true,
+      tipAmount: 10.0,
+      items: [
+        {
+          name: "Pizza",
+          price: 20.0,
+          eaters: [{ name: "Alice" }, { name: "Bob" }]
+        },
+        { name: "Salad", price: 10.0, eaters: [{ name: "Charlie" }] }
+      ],
+      eaters: [{ name: "Alice" }, { name: "Bob" }, { name: "Charlie" }]
+    }
+
+    const parseResult = formSchema.safeParse(inputData)
+    if (!parseResult.success) {
+      logZodErrors(parseResult.error, "FormSchema")
+    }
+    expect(parseResult.success).toBe(true)
+
+    if (parseResult.success) {
+      const result = calculateSplit(parseResult.data)
+
+      const splitParseResult = splitSchema.safeParse(result)
+      if (!splitParseResult.success) {
+        logZodErrors(splitParseResult.error, "SplitSchema")
+      }
+      expect(splitParseResult.success).toBe(true)
+    }
+  })
+
+  it("should correctly calculate the split for a given order", () => {
+    const input = {
+      checkName: "Wings Order",
+      taxAmount: 7.46,
+      tipAmount: 19.16,
+      tipBeforeTax: true,
+      items: [
+        {
+          name: "Large French Fries",
+          price: 7.49,
+          eaters: [
+            { name: "Vincent" },
+            { name: "Kyle" },
+            { name: "Samuel" },
+            { name: "Adam" }
+          ]
+        },
+        {
+          name: "30 Traditional Wings",
+          price: 40.99,
+          eaters: [
+            { name: "Vincent" },
+            { name: "Kyle" },
+            { name: "Samuel" },
+            { name: "Adam" }
+          ]
+        },
+        {
+          name: "10 Traditional Wings",
+          price: 15.99,
+          eaters: [{ name: "Karen" }]
+        },
+        {
+          name: "Michelob Ultra (2x)",
+          price: 13.5,
+          eaters: [{ name: "Adam" }, { name: "Kyle" }]
+        },
+        {
+          name: "20 Traditional Wings",
+          price: 28.49,
+          eaters: [
+            { name: "Vincent" },
+            { name: "Kyle" },
+            { name: "Samuel" },
+            { name: "Adam" }
+          ]
+        }
+      ],
+      eaters: [
+        { name: "Vincent" },
+        { name: "Karen" },
+        { name: "Samuel" },
+        { name: "Adam" },
+        { name: "Kyle" }
+      ]
+    }
+
+    const parseResult = formSchema.safeParse(input)
+    if (!parseResult.success) {
+      logZodErrors(parseResult.error, "FormSchema")
+    }
+    expect(parseResult.success).toBe(true)
+
+    if (parseResult.success) {
+      const split = calculateSplit(parseResult.data)
+
+      const splitParseResult = splitSchema.safeParse(split)
+      if (!splitParseResult.success) {
+        logZodErrors(splitParseResult.error, "SplitSchema")
+      }
+      expect(splitParseResult.success).toBe(true)
+
+      if (splitParseResult.success) {
+        expect(split.checkName).toBe(input.checkName)
+        expect(split.taxPercentage).toBeCloseTo(7, 1)
+        expect(split.taxAmount).toBe(input.taxAmount)
+        expect(split.tipPercentage).toBeGreaterThanOrEqual(15)
+        expect(split.tipPercentage).toBeLessThanOrEqual(20)
+        expect(split.tipAmount).toBe(input.tipAmount)
+
+        const eatersByName = split.eaters.reduce(
+          (acc, eater) => {
+            acc[eater.name] = eater
+            return acc
+          },
+          {} as Record<string, (typeof split.eaters)[0]>
+        )
+
+        const [karen, samuel, adam, vincent, kyle] = [
+          "Karen",
+          "Samuel",
+          "Adam",
+          "Vincent",
+          "Kyle"
+        ].map((name) => split.eaters.find((e) => e.name === name)!)
+
+        expect(karen.total).toBeLessThan(20)
+        expect(adam.total).toBeCloseTo(kyle.total)
+        expect(samuel.total).toBeCloseTo(vincent.total)
+        expect(samuel.total).toBeLessThan(adam.total)
+      }
     }
   })
 })
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { ZodError } from "zod"
-import { FormSchema } from "@/lib/schemas"
+import { EaterSchema, FormSchema, SplitSchema } from "@/lib/schemas"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -33,4 +33,74 @@ export function createSplitURL(
   const url = `${baseUrl}?${params.toString()}`
 
   return [url.replace(/(\[|\])/g, encodeURIComponent("$1")), params]
+}
+
+export function calculateSplit(data: FormSchema): SplitSchema {
+  const {
+    checkName,
+    taxAmount,
+    tipBeforeTax,
+    tipAmount,
+    items,
+    eaters: formEaters
+  } = data
+
+  const subTotal = items.reduce(
+    (acc: number, curr: { price: number }) => acc + curr.price,
+    0
+  )
+  const taxPercentage = (taxAmount / subTotal) * 100
+  const tipPercentage =
+    (tipAmount / (tipBeforeTax ? subTotal : subTotal + taxAmount)) * 100
+  const total = subTotal + taxAmount + tipAmount
+
+  const eaterMap = new Map<string, EaterSchema>()
+
+  // initialize eaters
+  formEaters.forEach(({ name }: { name: string }) => {
+    eaterMap.set(name.toLowerCase(), {
+      name,
+      taxAmount: 0,
+      tipAmount: 0,
+      total: 0,
+      subtotal: 0,
+      items: []
+    })
+  })
+
+  // calculate individual shares
+  items.forEach(
+    (item: { name: string; price: number; eaters: { name: string }[] }) => {
+      const share = item.price / item.eaters.length
+      item.eaters.forEach(({ name }: { name: string }) => {
+        const eater = eaterMap.get(name.toLowerCase())!
+        eater.subtotal += share
+        eater.items.push({ name: item.name })
+      })
+    }
+  )
+
+  // calculate tax, tip, and total for each eater
+  eaterMap.forEach((eater) => {
+    eater.taxAmount = eater.subtotal * (taxPercentage / 100)
+    eater.tipAmount =
+      (tipBeforeTax ? eater.subtotal : eater.subtotal + eater.taxAmount) *
+      (tipPercentage / 100)
+    eater.total = eater.subtotal + eater.taxAmount + eater.tipAmount
+  })
+
+  const split: SplitSchema = {
+    checkName,
+    taxPercentage,
+    taxAmount,
+    tipBeforeTax,
+    tipPercentage,
+    tipAmount,
+    subTotal,
+    total,
+    items,
+    eaters: Array.from(eaterMap.values())
+  }
+
+  return split
 }
