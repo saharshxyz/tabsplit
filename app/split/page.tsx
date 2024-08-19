f9b4ec733a3d86167bb25c4765ed8632c5d441ad
Saharsh Yeruva <yeruva@saharsh.xyz>
2024-08-18 20:39:19 -0400

    fix: add suspense boundary for `useSearchParams` in split page

--- app/split/page.tsx
+++ app/split/page.tsx
@@ -7,6 +7,7 @@ import { PenSquare } from "lucide-react"
 import { LoadingDisplay } from "./components/LoadingDisplay"
 import { useSearchParams } from "next/navigation"
 import Link from "next/link"
+import { Skeleton } from "@/components/ui/skeleton"
 
 const DynamicSplitContent = dynamic(
   () => import("./components/SplitComponent"),
@@ -16,21 +17,37 @@ const DynamicSplitContent = dynamic(
 )
 
 export default function Split() {
-  const searchParams = useSearchParams()
-
   return (
     <main className="mx-auto max-w-3xl">
-      <div className="m-2 flex justify-center">
-        <Link href={`/?${searchParams.toString()}`} className="w-full">
-          <Button className="w-full" variant="outline">
-            Edit Bill
-            <PenSquare className="ml-2 h-4 w-4" strokeWidth={2} />
-          </Button>
-        </Link>
-      </div>
+      <Suspense fallback={<EditBillButtonLoading />}>
+        <EditBillButton />
+      </Suspense>
       <Suspense fallback={<LoadingDisplay />}>
         <DynamicSplitContent />
       </Suspense>
     </main>
+  )
+}
+
+function EditBillButton() {
+  const searchParams = useSearchParams()
+
+  return (
+    <div className="m-2 flex justify-center">
+      <Link href={`/?${searchParams.toString()}`} className="w-full">
+        <Button className="w-full" variant="outline">
+          Edit Bill
+          <PenSquare className="ml-2 h-4 w-4" strokeWidth={2} />
+        </Button>
+      </Link>
+    </div>
+  )
+}
+
+function EditBillButtonLoading() {
+  return (
+    <div className="m-2 flex justify-center">
+      <Skeleton className="h-10 w-full rounded-md" />
+    </div>
   )
 }
