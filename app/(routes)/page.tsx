73e0b3872dc45145917dd4acccc4f066b1cbbf00
Saharsh Yeruva <yeruva@saharsh.xyz>
2024-08-20 05:00:11 -0400

    refactor: restructure project

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -5,20 +5,9 @@ import { Card, CardContent } from "@/components/ui/card"
 import { LoadingCard } from "@/components/LoadingCard"
 import { SplittingForm } from "./SplittingForm"
 import { useHash } from "@/lib/useHash"
-import { useEffect } from "react"
 
 export default function Home() {
   const { hash, parsedData, error } = useHash()
-
-  useEffect(() => {
-    console.log(hash)
-    console.log(parsedData)
-    console.log(error)
-    if (error == undefined || error != null) {
-      console.log(1)
-      console.error(error)
-    }
-  }, [hash, parsedData, error])
 
   return (
     <main className="mx-auto">
