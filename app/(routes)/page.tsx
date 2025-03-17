7766ea5c6cdddb13ab05950af8a78b7ce6551da9
Saharsh Yeruva <yeruva@saharsh.xyz>
2025-03-17 16:09:20 -0400

    ui: new light theme Credit to jnsahaj/tweakcn's Neo Brutalism theme

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
         </Link>
       </div>
 
-      <Card>
+      <Card className="mb-3">
         <CardContent>
           <TabForm initialData={parsedData} />
         </CardContent>
